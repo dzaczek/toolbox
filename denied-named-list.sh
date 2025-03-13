f4ad3d67c4985b37a9f530017b80201e2c3312dc
dzaczek <dzaczek@sysop.cat>
2025-03-13 09:33:01 +0100

    Create denied-named-list.sh

---
+++ denied-named-list.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+journalctl -u named | grep denied |grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | while read count ip; do
+
+    domain=$(dig -x $ip +short | tr "\n" " ")
+    if [ -z "$domain" ]; then
+        domain="(none )"
+    fi
+    printf "%-10s %-15s %s\n" "$count" "$ip" "$domain"
+done
