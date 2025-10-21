f9422705a7aa4f4d1fd0a03d7fa33c2b86c7cc2b
dzaczek <dzaczek@sysop.cat>
2025-10-21 10:13:38 +0200

    Refactor HDD controller script for clarity and efficiency

--- hdd_controler.sh
+++ hdd_controler.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#Test version 
 echo -e "DISK\tHOST\tPCI_CONTROLLER\tNAME"
 
-# --- handle SATA/SAS disks ---
+# SATA / SAS disks
 for dev in /sys/class/block/sd*; do
+  [[ -L "$dev" ]] || continue
   disk=$(basename "$dev")
   host=$(readlink -f "$dev" | grep -o 'host[0-9]\+' | head -1)
   [[ -z "$host" ]] && continue
-      pci_path=$(readlink -f /sys/class/scsi_host/$host | grep -o '/0000:[0-9a-f:.]\+' | tail -1)
-      pci_addr=$(basename "$pci_path")
-      name=$(lspci -s "$pci_addr" 2>/dev/null | cut -d' ' -f 2-)
-    printf "%s\t%s\t%s\t%s\n" "$disk" "$host" "$pci_addr" "$name"
+  fullpath=$(readlink -f /sys/class/scsi_host/$host)
+  pci_addr=$(echo "$fullpath" | grep -Eo '0000:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9]' | tail -1)
+  name=$(lspci -s "$pci_addr" 2>/dev/null | cut -d' ' -f 2-)
+  printf "%s\t%s\t%s\t%s\n" "$disk" "$host" "$pci_addr" "$name"
 done
 
-# --- handle NVMe disks ---
+# NVMe disks
 for dev in /sys/class/block/nvme*n*; do
-      disk=$(basename "$dev")
-  pci_path=$(readlink -f "$dev" | grep -o '/0000:[0-9a-f:.]\+' | tail -1)
-      pci_addr=$(basename "$pci_path")
+  [[ -L "$dev" ]] || continue
+  disk=$(basename "$dev")
+  fullpath=$(readlink -f "$dev")
+  pci_addr=$(echo "$fullpath" | grep -Eo '0000:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9]' | tail -1)
   name=$(lspci -s "$pci_addr" 2>/dev/null | cut -d' ' -f 2-)
   printf "%s\t%s\t%s\t%s\n" "$disk" "nvme" "$pci_addr" "$name"
 done
