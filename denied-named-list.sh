188b83d1680d39be952914ba2524abc39c9d4dad
dzaczek <dzaczek@sysop.cat>
2025-11-05 10:54:11 +0100

    Add DNS lookup for denied IPs in script

--- denied-named-list.sh
+++ denied-named-list.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#dns
 journalctl -u named | grep denied |grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort | uniq -c | sort -nr | while read count ip; do
 
     domain=$(dig -x $ip +short | tr "\n" " ")
